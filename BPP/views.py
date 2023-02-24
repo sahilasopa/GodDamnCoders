1e893175d2ee94da53bea4ccd97b5ece2cedb450
Sahil Asopa <sahilasopa12@gmail.com>
2023-02-25 00:03:25 +0530

    csrf

--- BPP/views.py
+++ BPP/views.py
@@ -3,60 +3,7 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-url = "https://dsep-protocol-client.becknprotocol.io/search"
-payload = json.dumps({
-    "context": {
-        "domain": "dsep:jobs",
-        "action": "search",
-        "version": "1.0.0",
-        "bap_id": "dsep-protocol.becknprotocol.io",
-        "bap_uri": "https://dsep-protocol-network.becknprotocol.io/",
-        "transaction_id": "a9aaecca-10b7-4d19-b640-b047a7c62195",
-        "message_id": "$89bdae17-9942-40c8-869a-5bd413356407",
-        "timestamp": "2022-10-11T09:55:41.161Z",
-        "ttl": "PT30S"
-    },
-    "message": {
-        "intent": {
-            "item": {
-                "descriptor": {
-                    "name": "Sahil Asopa"
-                },
-                "tags": [
-                    {
-                        "descriptor": {
-                            "name": "skill",
-                            "code": "skills"
-                        },
-                        "list": [
-                            {
-                                "descriptor": {
-                                    "code": "React"
-                                }
-                            }
-                        ]
-                    }
-                ]
-            }
-        }
-    }
-})
-headers = {
-    'Content-Type': 'application/json'
-}
-
 x = {
-    "context": {
-        "domain": "dsep:jobs",
-        "version": "1.0.0",
-        "action": "search",
-        "bap_id": "dsep-protocol.becknprotocol.io",
-        "bap_uri": "https://dsep-protocol-network.becknprotocol.io/",
-        "transaction_id": "a9aaecca-10b7-4d19-b640-b047a7c62195",
-        "message_id": "672f774c-4281-44dd-b1c2-84222a3d771e",
-        "ttl": "PT10M",
-        "timestamp": "2023-02-22T11:45:09.268Z"
-    },
     "responses": [
         {
             "context": {
