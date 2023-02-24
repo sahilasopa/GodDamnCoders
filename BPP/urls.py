b842563f6df57969364735d01cc2478c6add16bc
Sahil Asopa <sahilasopa12@gmail.com>
2023-02-24 21:44:18 +0530

    Initial commit

---
+++ BPP/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,include
+
+from BPP import views
+
+urlpatterns = [
+    path('apis/v1/search', views.Home), # https://goddamncoders.pythonanywhere.com/apis/v1/search
+
+]
---
+++ BPP/views.py
@@ -0,0 +1,53 @@
+import json
+
+from django.http import HttpResponse
+
+url = "https://dsep-protocol-client.becknprotocol.io/search"
+payload = json.dumps({
+    "context": {
+        "domain": "dsep:jobs",
+        "action": "search",
+        "version": "1.0.0",
+        "bap_id": "dsep-protocol.becknprotocol.io",
+        "bap_uri": "https://dsep-protocol-network.becknprotocol.io/",
+        "transaction_id": "a9aaecca-10b7-4d19-b640-b047a7c62195",
+        "message_id": "$89bdae17-9942-40c8-869a-5bd413356407",
+        "timestamp": "2022-10-11T09:55:41.161Z",
+        "ttl": "PT30S"
+    },
+    "message": {
+        "intent": {
+            "item": {
+                "descriptor": {
+                    "name": "Test"
+                },
+                "tags": [
+                    {
+                        "descriptor": {
+                            "name": "skill",
+                            "code": "skills"
+                        },
+                        "list": [
+                            {
+                                "descriptor": {
+                                    "code": "React"
+                                }
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+})
+headers = {
+    'Content-Type': 'application/json'
+}
+
+
+def Home(request):
+    if request.method == "POST":
+        print("=================== BODY ==========================")
+        print(request.body)
+        print("=================== BODY ==========================")
+    return HttpResponse("okay")
